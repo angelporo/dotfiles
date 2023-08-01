4698f1f5b2194cc439f0fafe4f9c2257bf489998
李渊 <liyuan@shidegroup.com>
2023-08-01 15:10:29 +0800

    添加librime模块

--- centaur-emacs/custom-post.el
+++ centaur-emacs/custom-post.el
@@ -27,6 +27,22 @@
 ;;
 ;;; Code:
 
+(add-to-list 'default-frame-alist '(undecorated . t))
+
+(defun setupEmacs29BindBuffer ()
+  (add-to-list 'auto-mode-alist '("\\(?:CMakeLists\\.txt\\|\\.cmake\\)\\'" . cmake-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.wxml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . typescript-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
+  )
+
+
+(when emacs/>=29p
+  (setupEmacs29BindBuffer)
+  )
 
 
 
@@ -35,22 +51,10 @@
   ("/Applications/SwitchKey.app" . "brew install --cask switchkey")
   :custom
   (rime-user-data-dir "~/Library/Rime/")
-  (rime-librime-root "~/elisp/librime/dist")
+  (rime-librime-root "~/.emacs.d/librime/dist")
   (rime-emacs-module-header-root "/usr/local/opt/emacs-plus@29/include")
-  :init
-  (defun my/rime-predicate-punctuation-next-char-is-paired-p ()
-    (if (not (eq (point) (point-max)))
-        (and (rime-predicate-current-input-punctuation-p)
-             (not (string-match-p
-                   (rx (any "\"\(\[\{"))
-                   (buffer-substring (point) (1- (point)))))
-             (string-match-p
-              (rx (any "\}\]\)\""))
-              (buffer-substring (point) (1+ (point)))))
-      nil))
-
-  ;; :hook
-  ;; (emacs-startup . (lambda () (setq default-input-method "rime")))
+  :hook
+  (emacs-startup . (lambda () (setq default-input-method "rime")))
   :bind
   ( :map rime-active-mode-map
     ;; 在已经激活 Rime 候选菜单时，强制在中英文之间切换，直到按回车。
@@ -70,7 +74,7 @@
     )
   :config
   ;; 在 modline 高亮输入法图标, 可用来快速分辨分中英文输入状态。
-  (setq mode-line-mule-info '((:eval (rime-lighter))))
+  ;; (setq mode-line-mule-info '((:eval (rime-lighter))))
   ;; 将如下快捷键发送给 rime，同时需要在 rime 的 key_binder/bindings 的部分配置才会生效。
   (add-to-list 'rime-translate-keybindings "C-h") ;; 删除拼音字符
   (add-to-list 'rime-translate-keybindings "C-d")
@@ -83,12 +87,10 @@
   (setq rime-disable-predicates
         '(rime-predicate-auto-english-p
           rime-predicate-after-alphabet-char-p
-          ;; rime-predicate-prog-in-code-p
           rime-predicate-ace-window-p
           rime-predicate-current-uppercase-letter-p
           rime-predicate-punctuation-after-ascii-p
-
-          my/rime-predicate-punctuation-next-char-is-paired-p
+          rime-predicate-prog-in-code-p
           ))
   (setq rime-show-candidate 'posframe)
   ;; (setq default-input-method "rime")
@@ -124,68 +126,10 @@
                         default-cursor-color)))
 
   (add-hook 'post-command-hook 'change-cursor-color-on-input-method)
-
-
   )
 
 
 
-(use-package pyim
-  :ensure t
-  :demand t
-  :config
-  ;; 我使用全拼
-  (setq pyim-default-scheme ')
-  (setq default-input-method "pyim")
-  ;; (use-package pyim-basedict
-  ;;   :ensure t
-  ;;   :config
-  ;;   ;; 加载 basedict 拼音词库。
-  ;;   (pyim-basedict-enable))
-
-  ;; 设置 pyim 探针
-  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
-  ;; 我自己使用的中英文动态切换规则是：
-  ;; 1. 光标只有在注释里面时，才可以输入中文。
-  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
-  (setq-default pyim-english-input-switch-functions
-                '(pyim-probe-program-mode
-                  pyim-probe-dynamic-english
-                  pyim-probe-auto-englishe
-                  pyim-probe-org-structure-template))
-
-  (setq-default pyim-punctuation-half-width-functions
-                '(pyim-probe-punctuation-line-beginning
-                  pyim-probe-punctuation-after-punctuation))
-  ;; ;; 开启代码搜索中文功能（比如拼音，五笔码等）
-  ;; (pyim-isearch-mode 1)
-  ;; 设置 pyim 是否使用云拼音
-  (setq pyim-cloudim 'baidu)
-
-  ;; (setq pyim-dicts
-  ;;       '((:name "dict1" :file "~/elisp/scel2pyim/前端工程师必备词库.pyim")
-  ;;         (:name "dict2" :file "~/elisp/scel2pyim/开发大神专用词库【官方推荐】.pyim")
-  ;;         (:name "dict3" :file "~/elisp/scel2pyim/实用IT词汇.pyim")
-  ;;         (:name "dict4" :file "~/elisp/scel2pyim/编程开发.pyim")
-  ;;         ))
-
-  ;; 选词框显示5个候选词
-  (setq pyim-page-length 9)
-
-  ;; 让 Emacs 启动时自动加载 pyim 词库
-  ;; (add-hook 'emacs-startup-hook
-  ;;           #'(lambda () (pyim-restart-1 t)))
-  (global-set-key (kbd "M-j") 'pyim-convert-string-at-point)
-  )
-
-
-
-
-
-
-
-
-(add-to-list 'default-frame-alist '(undecorated-round . t))
 
 (defun start-centaur-bind-keys ()
   (global-set-key (kbd "M-s") 'save-buffer)
@@ -209,20 +153,20 @@
   :ensure nil
   :load-path "~/elisp/lsp-bridge"
   :hook (prog-mode . lsp-bridge-mode)
-  ;; :bind (:map lsp-bridge-mode
-  ;;        ("C-s-j" . lsp-bridge-jump-to-next-diagnostic) ;显示下一个错误
-  ;;        ("C-s-k" . lsp-bridge-jump-to-prev-diagnostic) ;显示上一个错误
-  ;;        ("C-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
-  ;;        ("C-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
-  ;;        ("C-c e l" . lsp-bridge-list-diagnostics)
-  ;;        ("C-s-u" . lsp-bridge-ignore-current-diagnostic) ;插入注视忽略当前诊断
-  ;;        ("M-." . lsp-bridge-find-def)
-  ;;        ("M-," . lsp-bridge-find-def-return)
-  ;;        ("M-?" . lsp-bridge-find-references)
-  ;;        ("C-c RET" . lsp-bridge-popup-documentation)
-  ;;        ("C-c m" . lsp-bridge-rename)
-  ;;        ("M-RET" . lsp-bridge-code-action)
-  ;;        )
+  :bind (:map lsp-bridge-mode
+         ("C-s-j" . lsp-bridge-jump-to-next-diagnostic) ;显示下一个错误
+         ("C-s-k" . lsp-bridge-jump-to-prev-diagnostic) ;显示上一个错误
+         ("C-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
+         ("C-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
+         ("C-c e l" . lsp-bridge-list-diagnostics)
+         ("C-s-u" . lsp-bridge-ignore-current-diagnostic) ;插入注视忽略当前诊断
+         ("M-." . lsp-bridge-find-def)
+         ("M-," . lsp-bridge-find-def-return)
+         ("M-?" . lsp-bridge-find-references)
+         ("C-c RET" . lsp-bridge-popup-documentation)
+         ("C-c m" . lsp-bridge-rename)
+         ("M-RET" . lsp-bridge-code-action)
+         )
   :config
   (setq acm-enable-tabnine nil)
   (setq acm-enable-codeium nil)
@@ -237,41 +181,9 @@
                                                       (("html") . "html_emmet")
                                                       (("tsx") . "tsx_emmet")
                                                       ))
-  ;; 增加 treesit 相关的 xx-ts-mode
-  (setq lsp-bridge-single-lang-server-mode-list
-        '(((c-mode c-ts-mode c++-mode c++-ts-mode objc-mode) . lsp-bridge-c-lsp-server)
-          (cmake-mode . "cmake-language-server")
-          ((java-mode java-ts-mode) . "jdtls")
-          ((python-mode python-ts-mode) . lsp-bridge-python-lsp-server)
-          ((go-mode go-ts-mode) . "gopls")
-          ((js2-mode js-mode js-ts-mode rjsx-mode) . "javascript")
-          (typescript-tsx-mode . "typescriptreact")
-          ((typescript-mode typescript-ts-mode) . "typescript")
-          ((latex-mode Tex-latex-mode texmode context-mode texinfo-mode bibtex-mode) . lsp-bridge-tex-lsp-server)
-          ((sh-mode bash-ts-mode) . "bash-language-server")
-          ((css-mode css-ts-mode) . "vscode-css-language-server")
-          ((yaml-mode yaml-ts-mode) . "yaml-language-server")
-          ((json-mode json-ts-mode) . "vscode-json-language-server")
-          ((dockerfile-mode dockerfile-ts-mode) . "docker-langserver")))
   ;;; lsp-bridge
   ;; M-j 被预留给 pyim 使用。
   (define-key acm-mode-map (kbd "M-j") nil)
-  ;; 使用 TAB 而非回车键来选定。
-  ;;(define-key acm-mode-map (kbd "RET") nil)
-  (define-key lsp-bridge-mode-map (kbd "M-.") #'lsp-bridge-find-def)
-  (define-key lsp-bridge-mode-map (kbd "C-M-.") #'lsp-bridge-find-def-other-window)
-  (define-key lsp-bridge-mode-map (kbd "M-,") #'lsp-bridge-find-def-return)
-  (define-key lsp-bridge-mode-map (kbd "M-?") #'lsp-bridge-find-references)
-  (define-key lsp-bridge-mode-map (kbd "M-d") #'lsp-bridge-popup-documentation)
-  ;; 这两个快捷键让位于 symobl-overlay
-  ;; (define-key lsp-bridge-mode-map (kbd "M-n") #'lsp-bridge-popup-documentation-scroll-up)
-  ;; (define-key lsp-bridge-mode-map (kbd "M-p") #'lsp-bridge-popup-documentation-scroll-down)
-  (define-key lsp-bridge-mode-map (kbd "C-c C-a") #'lsp-bridge-code-action)
-  (define-key lsp-bridge-mode-map (kbd "C-c C-f") #'lsp-bridge-code-format)
-  (define-key lsp-bridge-mode-map (kbd "C-s-d") #'lsp-bridge-diagnostic-list)
-  (define-key lsp-bridge-mode-map (kbd "C-s-n") #'lsp-bridge-diagnostic-jump-next)
-  (define-key lsp-bridge-mode-map (kbd "C-s-p") #'lsp-bridge-diagnostic-jump-prev)
-
   ;; 这些字符的后面不再弹出补全菜单
   (setq lsp-bridge-completion-hide-characters '("%" ":" ";" "(" ")" "[" "]" "{" "}" "," "=" ">" "\""))
   (global-lsp-bridge-mode)
@@ -288,6 +200,41 @@
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion))))
   )
+
+
+
+
+(setq prettier-js-args '(
+                         ;; 尽可能尾随逗号
+                         "--trailing-comma" "all"
+                         ;; { foo: bar }
+                         "--bracket-spacing" "false"
+                         ;; 尽可能省略括号。例子：x => x
+                         "--arrow-parens" "avoid"
+                         ;; 指定打印机将环绕的行长度。
+                         "--print-width" "100"
+                         ;; 指定每个缩进级别的空格数。
+                         "--tab-width" "2"
+                         ;; 在每条语句的末尾添加一个分号。
+                         "--no-semi" "true"
+                         ;; 在 JSX 中使用单引号代替双引号。
+                         "--jsx-single-quote" "true"
+                         ;; 将>多行 JSX 元素的 放在最后一行的末尾，而不是单独放在下一行（不适用于自关闭元素）。
+                         "--jsx-bracket-same-line" "true"
+                         ))
+
+(use-package add-node-modules-path
+  :ensure t
+  :config
+  (eval-after-load 'js-mode
+    '(add-hook 'js-mode-hook #'add-node-modules-path))
+  (eval-after-load 'web-mode
+    '(progn
+       (add-hook 'web-mode-hook #'add-node-modules-path)
+       (add-hook 'web-mode-hook #'prettier-js-mode)
+       ))
+  )
+
 
 ;; (use-package flutter
 ;;   :after dart-mode
