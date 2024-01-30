b43bc4947de50772513fc6e751eb3839cb96fd80
李渊 <liyuan@shidegroup.com>
2024-01-30 14:16:38 +0800

    使用lsp-bridge

--- centaur-emacs/custom-post.el
+++ centaur-emacs/custom-post.el
@@ -27,7 +27,14 @@
 ;;
 ;;; Code:
 
-(add-to-list 'default-frame-alist '(undecorated . t))
+
+
+(defun my/toggle-transparency ()
+  (interactive)
+  (set-frame-parameter (selected-frame) 'alpha '(90 . 90)) ;; 分别为 frame 获得焦点和失去焦点的不透明度。
+  (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+  )
+
 
 (defun setupEmacs29BindBuffer ()
   (add-to-list 'auto-mode-alist '("\\(?:CMakeLists\\.txt\\|\\.cmake\\)\\'" . cmake-ts-mode))
@@ -36,13 +43,119 @@
   (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
   (add-to-list 'auto-mode-alist '("\\.js\\'" . typescript-ts-mode))
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.less\\'" . less-css-mode))
   (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
+
+  (add-hook 'typescript-ts-mode-hook 'prettier-mode)
+  (add-hook 'tsx-ts-mode-hook 'prettier-mode)
+  (add-hook 'js2-mode-hook 'prettier-mode)
+  (add-hook 'web-mode-hook 'prettier-mode)
+  (add-hook 'css-mode-hook 'prettier-mode)
+
   )
 
+(defun start-centaur-bind-keys ()
+  (global-set-key (kbd "C-a") 'beginning-of-line)
+  ;; (global-set-key (kbd "C-e") 'end-of-line)
+  (global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
+  (global-set-key (kbd "C-i") 'yas-expand)
+  (global-set-key (kbd "C-c i") 'project-find-file)
+  (global-set-key (kbd "M-RET") 'eglot-code-actions)
+  (global-set-key (kbd "M-s") 'save-buffer)
+  )
 
 (when emacs/>=29p
   (setupEmacs29BindBuffer)
+  (start-centaur-bind-keys)
+  (add-to-list 'default-frame-alist '(undecorated . t))
+
+  ;; (my/toggle-transparency)
   )
+
+
+
+(use-package emacs
+  :init
+  ;; 在 minibuffer 中不显示光标。
+  (setq minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook (lambda () (setq default-input-method "rime")))
+  )
+
+
+
+
+(use-package lsp-bridge
+  :ensure nil
+  :load-path "~/elisp/lsp-bridge"
+  :hook (prog-mode . lsp-bridge-mode)
+  ;; :bind (:map lsp-bridge-mode
+  ;;        ("C-s-j" . lsp-bridge-jump-to-next-diagnostic) ;显示下一个错误
+  ;;        ("C-s-k" . lsp-bridge-jump-to-prev-diagnostic) ;显示上一个错误
+  ;;        ("C-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
+  ;;        ("C-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
+  ;;        ("C-c e l" . lsp-bridge-list-diagnostics)
+  ;;        ("C-s-u" . lsp-bridge-ignore-current-diagnostic) ;插入注视忽略当前诊断
+  ;;        ("M-." . lsp-bridge-find-def)
+  ;;        ("M-," . lsp-bridge-find-def-return)
+  ;;        ("M-?" . lsp-bridge-find-references)
+  ;;        ("C-c RET" . lsp-bridge-popup-documentation)
+  ;;        ("C-c m" . lsp-bridge-rename)
+  ;;        ("M-RET" . lsp-bridge-code-action)
+  ;;        )
+  :config
+  (setq acm-enable-tabnine nil)
+  (setq acm-enable-codeium nil)
+  (setq acm-enable-yas nil)
+  (setq acm-enable-tempel nil)
+  (setq lsp-bridge-auto-format-code-idle -1)
+  (setq lsp-bridge-enable-hover-diagnostic t)
+  (setq lsp-bridge-enable-auto-format-code nil)
+  (setq acm-backend-yas-candidates-number 4)
+  (setq lsp-bridge-multi-lang-server-extension-list '((("less") . "css_emmet")
+                                                      (("vue") . "volar_emmet")
+                                                      (("html") . "html_emmet")
+                                                      (("tsx") . "tsx_emmet")
+                                                      ))
+  ;; 增加 treesit 相关的 xx-ts-mode
+  (setq lsp-bridge-single-lang-server-mode-list
+        '(((c-mode c-ts-mode c++-mode c++-ts-mode objc-mode) . lsp-bridge-c-lsp-server)
+          (cmake-mode . "cmake-language-server")
+          ((java-mode java-ts-mode) . "jdtls")
+          ((python-mode python-ts-mode) . lsp-bridge-python-lsp-server)
+          ((go-mode go-ts-mode) . "gopls")
+          ((js2-mode js-mode js-ts-mode rjsx-mode) . "javascript")
+          (typescript-tsx-mode . "typescriptreact")
+          ((typescript-mode typescript-ts-mode) . "typescript")
+          ((latex-mode Tex-latex-mode texmode context-mode texinfo-mode bibtex-mode) . lsp-bridge-tex-lsp-server)
+          ((sh-mode bash-ts-mode) . "bash-language-server")
+          ((css-mode css-ts-mode) . "vscode-css-language-server")
+          ((yaml-mode yaml-ts-mode) . "yaml-language-server")
+          ((json-mode json-ts-mode) . "vscode-json-language-server")
+          ((dockerfile-mode dockerfile-ts-mode) . "docker-langserver")))
+  ;;; lsp-bridge
+  ;; M-j 被预留给 pyim 使用。
+  (define-key acm-mode-map (kbd "M-j") nil)
+  ;; 使用 TAB 而非回车键来选定。
+  ;;(define-key acm-mode-map (kbd "RET") nil)
+  (define-key lsp-bridge-mode-map (kbd "M-.") #'lsp-bridge-find-def)
+  (define-key lsp-bridge-mode-map (kbd "C-M-.") #'lsp-bridge-find-def-other-window)
+  (define-key lsp-bridge-mode-map (kbd "M-,") #'lsp-bridge-find-def-return)
+  (define-key lsp-bridge-mode-map (kbd "M-?") #'lsp-bridge-find-references)
+  ;; (define-key lsp-bridge-mode-map (kbd "M-d") #'lsp-bridge-popup-documentation)
+  ;; 这两个快捷键让位于 symobl-overlay
+  ;; (define-key lsp-bridge-mode-map (kbd "M-n") #'lsp-bridge-popup-documentation-scroll-up)
+  ;; (define-key lsp-bridge-mode-map (kbd "M-p") #'lsp-bridge-popup-documentation-scroll-down)
+  (define-key lsp-bridge-mode-map (kbd "M-RET") #'lsp-bridge-code-action)
+  (define-key lsp-bridge-mode-map (kbd "C-c C-f") #'lsp-bridge-code-format)
+  (define-key lsp-bridge-mode-map (kbd "C-s-d") #'lsp-bridge-diagnostic-list)
+  (define-key lsp-bridge-mode-map (kbd "C-s-n") #'lsp-bridge-diagnostic-jump-next)
+  (define-key lsp-bridge-mode-map (kbd "C-s-p") #'lsp-bridge-diagnostic-jump-prev)
+
+  ;; 这些字符的后面不再弹出补全菜单
+  (setq lsp-bridge-completion-hide-characters '("%" ":" ";" "(" ")" "[" "]" "{" "}" "," "=" ">" "\""))
+  (global-lsp-bridge-mode)
+  )
+
 
 
 
@@ -51,28 +164,33 @@
   ("/Applications/SwitchKey.app" . "brew install --cask switchkey")
   :custom
   (rime-user-data-dir "~/Library/Rime/")
+  ;; (rime-share-data-dir "~/Library/Rime/")
   (rime-librime-root "~/.emacs.d/librime/dist")
   (rime-emacs-module-header-root "/usr/local/opt/emacs-plus@29/include")
   :hook
   (emacs-startup . (lambda () (setq default-input-method "rime")))
   :bind
-  ( :map rime-active-mode-map
-    ;; 在已经激活 Rime 候选菜单时，强制在中英文之间切换，直到按回车。
-    ("M-j" . 'rime-inline-ascii)
-    :map rime-mode-map
-    ;; 强制切换到中文模式
-    ("M-j" . 'rime-force-enable)
-    ;; 下面这些快捷键需要发送给 rime 来处理, 需要与 default.custom.yaml 文件中的 key_binder/bindings 配置相匹配。
-    ;; 中英文切换
-    ("C-." . 'rime-send-keybinding)
-    ;; 输入法菜单
-    ("C-+" . 'rime-send-keybinding)
-    ;; 中英文标点切换
-    ("C-," . 'rime-send-keybinding)
-    ;; 全半角切换
-    ;; ("C-," . 'rime-send-keybinding)
-    )
+  (:map rime-active-mode-map
+   ;; 在已经激活 Rime 候选菜单时，强制在中英文之间切换，直到按回车。
+   ("M-j" . 'rime-inline-ascii)
+   :map rime-mode-map
+   ;; ;; 强制切换到中文模式
+   ("M-j" . 'rime-force-enable)
+   ;; 下面这些快捷键需要发送给 rime 来处理, 需要与 default.custom.yaml 文件中的 key_binder/bindings 配置相匹配。
+   ;; 中英文切换
+   ("C-." . 'rime-send-keybinding)
+   ;; 输入法菜单
+   ("C-+" . 'rime-send-keybinding)
+   ;; 中英文标点切换
+   ("C-," . 'rime-send-keybinding)
+   ;; 菜单
+   ;; ("C-`" . 'rime-send-keybinding)
+
+   ;; 全半角切换
+   ;; ("C-," . 'rime-send-keybinding)
+   )
   :config
+
   ;; 在 modline 高亮输入法图标, 可用来快速分辨分中英文输入状态。
   ;; (setq mode-line-mule-info '((:eval (rime-lighter))))
   ;; 将如下快捷键发送给 rime，同时需要在 rime 的 key_binder/bindings 的部分配置才会生效。
@@ -93,7 +211,6 @@
           rime-predicate-prog-in-code-p
           ))
   (setq rime-show-candidate 'posframe)
-  ;; (setq default-input-method "rime")
 
   (setq rime-posframe-properties
         (list :background-color "#333333"
@@ -105,7 +222,8 @@
     (if (or (string-match "vterm-mode" (symbol-name major-mode))
             (string-match "dired-mode" (symbol-name major-mode))
             (string-match "image-mode" (symbol-name major-mode))
-            (string-match "minibuffer-mode" (symbol-name major-mode)))
+            (string-match "minibuffer-mode" (symbol-name major-mode))
+            )
         (activate-input-method nil)
       (activate-input-method "rime")))
 
@@ -131,108 +249,30 @@
 
 
 
-(defun start-centaur-bind-keys ()
-  (global-set-key (kbd "M-s") 'save-buffer)
-  (global-set-key (kbd "C-a") 'beginning-of-line)
-  (global-set-key (kbd "C-e") 'end-of-line)
-  (global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
-  (global-set-key (kbd "C-i") 'yas-expand)
-  )
-
-(start-centaur-bind-keys)
-
 (setq counsel-ag-base-command '(
                                 "ag"
                                 "--vimgrep" "%s"
                                 "--ignore" "*node_modules*"
                                 ))
-(setenv "LANG" "Chinese")
 
-
-(use-package lsp-bridge
-  :ensure nil
-  :load-path "~/elisp/lsp-bridge"
-  :hook (prog-mode . lsp-bridge-mode)
-  :bind (:map lsp-bridge-mode
-         ("C-s-j" . lsp-bridge-jump-to-next-diagnostic) ;显示下一个错误
-         ("C-s-k" . lsp-bridge-jump-to-prev-diagnostic) ;显示上一个错误
-         ("C-s-n" . lsp-bridge-popup-documentation-scroll-up) ;向下滚动文档
-         ("C-s-p" . lsp-bridge-popup-documentation-scroll-down) ;向上滚动文档
-         ("C-c e l" . lsp-bridge-list-diagnostics)
-         ("C-s-u" . lsp-bridge-ignore-current-diagnostic) ;插入注视忽略当前诊断
-         ("M-." . lsp-bridge-find-def)
-         ("M-," . lsp-bridge-find-def-return)
-         ("M-?" . lsp-bridge-find-references)
-         ("C-c RET" . lsp-bridge-popup-documentation)
-         ("C-c m" . lsp-bridge-rename)
-         ("M-RET" . lsp-bridge-code-action)
-         )
-  :config
-  (setq acm-enable-tabnine nil)
-  (setq acm-enable-codeium nil)
-  (setq acm-enable-yas nil)
-  (setq acm-enable-tempel nil)
-  (setq lsp-bridge-auto-format-code-idle -1)
-  (setq lsp-bridge-enable-hover-diagnostic t)
-  (setq lsp-bridge-enable-auto-format-code nil)
-  (setq acm-backend-yas-candidates-number 4)
-  (setq lsp-bridge-multi-lang-server-extension-list '((("less") . "css_emmet")
-                                                      (("vue") . "volar_emmet")
-                                                      (("html") . "html_emmet")
-                                                      (("tsx") . "tsx_emmet")
-                                                      ))
-  ;;; lsp-bridge
-  ;; M-j 被预留给 pyim 使用。
-  (define-key acm-mode-map (kbd "M-j") nil)
-  ;; 这些字符的后面不再弹出补全菜单
-  (setq lsp-bridge-completion-hide-characters '("%" ":" ";" "(" ")" "[" "]" "{" "}" "," "=" ">" "\""))
-  (global-lsp-bridge-mode)
+(use-package emmet
+  :ensure t
+  :hook ((typescript-ts-mode  tsx-ts-mode web-mode) . emmet-mode)
   )
+
+
+;; (use-package vue-mode
+;;   :ensure t
+;;   :mode "\\.vue\\$"
+;;   :config
+;;   (add-hook 'mmm-mode-hook
+;;             (lambda ()
+;;               (set-face-background 'mmm-default-submode-face nil)))
+;;   )
+
 
 (use-package ag
   :ensure t
-  )
-
-
-(use-package orderless
-  :ensure t
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion))))
-  )
-
-
-
-
-(setq prettier-js-args '(
-                         ;; 尽可能尾随逗号
-                         "--trailing-comma" "all"
-                         ;; { foo: bar }
-                         "--bracket-spacing" "false"
-                         ;; 尽可能省略括号。例子：x => x
-                         "--arrow-parens" "avoid"
-                         ;; 指定打印机将环绕的行长度。
-                         "--print-width" "100"
-                         ;; 指定每个缩进级别的空格数。
-                         "--tab-width" "2"
-                         ;; 在每条语句的末尾添加一个分号。
-                         "--no-semi" "true"
-                         ;; 在 JSX 中使用单引号代替双引号。
-                         "--jsx-single-quote" "true"
-                         ;; 将>多行 JSX 元素的 放在最后一行的末尾，而不是单独放在下一行（不适用于自关闭元素）。
-                         "--jsx-bracket-same-line" "true"
-                         ))
-
-(use-package add-node-modules-path
-  :ensure t
-  :config
-  (eval-after-load 'js-mode
-    '(add-hook 'js-mode-hook #'add-node-modules-path))
-  (eval-after-load 'web-mode
-    '(progn
-       (add-hook 'web-mode-hook #'add-node-modules-path)
-       (add-hook 'web-mode-hook #'prettier-js-mode)
-       ))
   )
 
 
